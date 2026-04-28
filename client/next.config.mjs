27c85e1ec47993ff1c59dbbd125f9ebbeefc3a66
Megh <meghd9@gmail.com>
2026-04-28 10:39:09 +0100

    Clean up README and format Next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -7,9 +7,9 @@ const nextConfig = {
         hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
         port: "",
         pathname: "/**",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default nextConfig;
